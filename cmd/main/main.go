b1ba3fc8cee520de41055e267d91e2bfcdc9ca6b
cjohansen11 <cjohansen11@me.com>
2023-01-13 13:58:49 -0500

    Added main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/cjohansen11/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:3002", r))
+}
